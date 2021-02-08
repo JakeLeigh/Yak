7844faf4dd0b017d52c06292e1f84fb43551ab53
Jake Leigh <jake@jakeleigh.io>
2021-02-08 10:29:04 +0000

    Update styling for learn more comp

--- Yak/Yak/ClientApp/src/components/LearnMore/LearnMore.js
+++ Yak/Yak/ClientApp/src/components/LearnMore/LearnMore.js
@@ -8,8 +8,8 @@ class LearnMore extends React.Component{
       <div className='LearnMoreContainer'>
         <div class='learnMoreDiv'>
           <div class='container learnMoreContent'>
-            <h2>We're here to help you thrive</h2>
-            <p>The remote working revolution has arrived, but it’s okay if video meetings from the spare room, cupboard or dining table aren’t working for your company. Everyone deserves to work from a space that works for them, give your employees the freedom they have been enjoying, whilst you benefit from their increased productivity.</p>
+            <h2 class='learnMoreTitle'>We're here to help you thrive</h2>
+            <p class='learnMoreText'>The remote working revolution has arrived, but it’s okay if video meetings from the spare room, cupboard or dining table aren’t working for your company. Everyone deserves to work from a space that works for them, give your employees the freedom they have been enjoying, whilst you benefit from their increased productivity.</p>
             <button class='yakButton joinButton'>Learn More</button>
           </div>
           <div>
