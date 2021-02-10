752ec8c648bb92de5955059dc3f4182d44f5566f
Jake Leigh <jake@jakeleigh.io>
2021-02-10 10:38:02 +0000

    Create mobile hamburger menu nav

--- Yak/Yak/ClientApp/src/components/Nav/Nav.js
+++ Yak/Yak/ClientApp/src/components/Nav/Nav.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Nav.scss';
 
 class Nav extends React.Component{
-  state = { 
-    toggleMobileNav: false
-  };
+state = { 
+  toggleMobileNav: false
+};
 
-  toggleMenu = () => {
-    console.log('got here');
-    this.setState({
-      toggleMobileNav: !this.state.toggleMobileNav
-    });
-  }
+toggleMenu = () => {
+  console.log('got here');
+  this.setState({
+    toggleMobileNav: !this.state.toggleMobileNav
+  });
+}
 render(){
 
-  return (
-    /*<nav className='nav'>
+return (
+  <Fragment>
+    <nav className='nav showOnDesktop'>
       <div className='nav-wrapper'>
         <div className='nav-inner container-fluid'>
           <a href='/' class='navTitle' data-aos="fade-up">YAK</a>
@@ -23,22 +24,21 @@ render(){
           <button className='yakButton joinButton' data-aos="fade-up">Join Now</button>
         </div>
       </div>
-    </nav>*/
-    <nav class="Navigation">
-<h2 class="Navigation-smallLogo">PLAY ULTI</h2>
-<div class={this.state.toggleMobileNav===true ? 'Overlay--on Overlay' : 'Overlay'}>
-  <ul class="Navigation-list">
-    <li><a href="#">PLAY</a></li>
-    <li><a href="#">TEACH</a></li>
-    <li><a href="#">SPIRIT</a></li>
-    <li><a href="#">GIVE</a></li>
-  </ul>
-</div>
-<section class="Navigation--collapsed" onClick={this.toggleMenu}>
-  <div class={this.state.toggleMobileNav===true ? 'BurgerMenu--on BurgerMenu' : 'BurgerMenu'}></div>
-</section>
-</nav>
-  )
+    </nav>
+    <nav class="Navigation showOnMobile">
+      <a href='/' class='navTitle' data-aos="fade-up">YAK</a>
+      <div class={this.state.toggleMobileNav===true ? 'Overlay--on Overlay' : 'Overlay'}>
+        <ul class="Navigation-list">
+          <li><a class='navAbout' data-aos="fade-up">About</a></li>
+          <li><button className='yakButtonMobile joinButton' data-aos="fade-up">Join Now</button></li>
+        </ul>
+      </div>
+      <section class="Navigation--collapsed" onClick={this.toggleMenu}>
+        <div class={this.state.toggleMobileNav===true ? 'BurgerMenu--on BurgerMenu' : 'BurgerMenu'}></div>
+      </section>
+    </nav>
+  </Fragment>
+)
 }
 }
 
